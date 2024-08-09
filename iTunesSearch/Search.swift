2e870078d8ff39ba09987019132e255f01d8d5fa
rkdtnlzl <rkdtnlzl@naver.com>
2024-08-09 14:46:41 +0900

    [Feat] MusicVideo Model 생성

---
+++ iTunesSearch/Search.swift
@@ -0,0 +1,18 @@
+//
+//  Search.swift
+//  iTunesSearch
+//
+//  Created by 강석호 on 8/8/24.
+//
+
+import Foundation
+
+struct Search: Decodable {
+    let results: [SearchResult]
+}
+
+struct SearchResult: Decodable {
+    let artworkUrl100: String?
+    let trackName: String?
+    let artistName: String?
+}
