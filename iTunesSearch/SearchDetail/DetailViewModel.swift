77ba8307f0f663633172f4ffc0d2a62b0db64fc1
rkdtnlzl <rkdtnlzl@naver.com>
2024-08-10 16:29:41 +0900

    [Feat] previewURL ViewModel 전달

--- iTunesSearch/SearchDetail/DetailViewModel.swift
+++ iTunesSearch/SearchDetail/DetailViewModel.swift
@@ -13,10 +13,12 @@ class DetailViewModel {
     let trackName: String
     let artistName: String
     let artworkUrl100: String?
+    let previewUrl: String?
     
-    init(trackName: String, artistName: String, artworkUrl100: String?) {
+    init(trackName: String, artistName: String, artworkUrl100: String?, previewUrl: String?) {
         self.trackName = trackName
         self.artistName = artistName
         self.artworkUrl100 = artworkUrl100
+        self.previewUrl = previewUrl
     }
 }
