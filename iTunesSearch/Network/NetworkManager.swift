ca00999f787a0cff9b0079425317f70e3a23c609
rkdtnlzl <rkdtnlzl@naver.com>
2024-08-12 22:52:25 +0900

    [Feat] Single을 활용한 네트워크 관리

--- iTunesSearch/Network/NetworkManager.swift
+++ iTunesSearch/Network/NetworkManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 import RxSwift
 
 enum APIError: Error {
@@ -19,39 +20,63 @@ final class NetworkManager {
     
     private init() { }
     
-    func fetchSearch(term: String) -> Observable<Search>{
-        let url = "https://itunes.apple.com/search?term=\(term)&entity=musicVideo"
+    func fetchSearch(term: String) -> Single<Search> {
+        let urlString = "https://itunes.apple.com/search?term=\(term)&entity=musicVideo"
         
-        let result = Observable<Search>.create { observer in
-            guard let url = URL(string: url) else {
-                observer.onError(APIError.invalidURL)
+        return Single.create { observer -> Disposable in
+            guard let url = URL(string: urlString) else {
+                observer(.failure(APIError.invalidURL))
                 return Disposables.create()
             }
             
-            URLSession.shared.dataTask(with: url) {data, response, error in
-                if error != nil {
-                    observer.onError(APIError.unknownResponse)
-                    return
+            AF.request(url)
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: Search.self) { response in
+                    switch response.result {
+                    case .success(let searchResult):
+                        observer(.success(searchResult))
+                    case .failure:
+                        observer(.failure(APIError.unknownResponse))
+                    }
                 }
-                
-                guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-                    observer.onError(APIError.statusError)
-                    return
-                }
-                
-                if let data = data,
-                   let appData = try? JSONDecoder().decode(Search.self, from: data) {
-                    observer.onNext(appData)
-                    observer.onCompleted()
-                } else {
-                    print("응답이 왔으나 디코딩 실패")
-                    observer.onError(APIError.unknownResponse)
-                }
-            }.resume()
             
             return Disposables.create()
         }.debug("조회")
-        
-        return result
     }
+    
+    //    func fetchSearch(term: String) -> Observable<Search>{
+    //        let url = "https://itunes.apple.com/search?term=\(term)&entity=musicVideo"
+    //
+    //        let result = Observable<Search>.create { observer in
+    //            guard let url = URL(string: url) else {
+    //                observer.onError(APIError.invalidURL)
+    //                return Disposables.create()
+    //            }
+    //
+    //            URLSession.shared.dataTask(with: url) {data, response, error in
+    //                if error != nil {
+    //                    observer.onError(APIError.unknownResponse)
+    //                    return
+    //                }
+    //
+    //                guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+    //                    observer.onError(APIError.statusError)
+    //                    return
+    //                }
+    //
+    //                if let data = data,
+    //                   let appData = try? JSONDecoder().decode(Search.self, from: data) {
+    //                    observer.onNext(appData)
+    //                    observer.onCompleted()
+    //                } else {
+    //                    print("응답이 왔으나 디코딩 실패")
+    //                    observer.onError(APIError.unknownResponse)
+    //                }
+    //            }.resume()
+    //
+    //            return Disposables.create()
+    //        }.debug("조회")
+    //
+    //        return result
+    //    }
 }
